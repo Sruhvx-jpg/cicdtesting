9b882ce9f351b4638aa03654df02afd49bbd6445
dron <sarikarn120@gmail.com>
2026-06-08 15:03:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import "dotenv/config"
+
+
+const app = express()
+const PORT  = process.env.PORT ?? 8000
+
+app.get('/', (req, res) => {
+    return res.json({message: "server is healthy"})
+})
+
+app.listen(PORT, () => console.log("server is running on PORT:", process.env.PORT))
